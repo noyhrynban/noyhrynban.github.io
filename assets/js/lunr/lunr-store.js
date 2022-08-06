e644e4ff58d10cdb9d79788effce7e018f35eea8
noyhrynban <noyhrynban@users.noreply.github.com>
2022-08-06 10:51:26 +0000

    jekyll build from Action 544d94aea325249bf6243e0f15c5b65a66057284

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "New Site",
+        "excerpt":"Foo  ","categories": [],
+        "tags": [],
+        "url": "/2019/03/27/new-site.html",
+        "teaser": null
+      }]
